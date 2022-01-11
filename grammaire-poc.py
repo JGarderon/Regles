ab474b1bfdac98d70da21866935644d28e500073
Administrator <julien.garderon@gmail.com>
2022-01-11 21:49:47 +0100

    poc de grammaire fonctionnel dont instanciation des variables pour l'exemple

--- grammaire-poc.py
+++ grammaire-poc.py
@@ -12,50 +12,85 @@ class Traducteur:
 
 	jetons = [] 
 
-	def __espaces__( self, position ): 
-		for i, signe in enumerate( self.source[position:] ): 
-			if signe in ( ' ', '\t', '\n' ): 
-				continue 
-			else: 
-				return i 
-		return i 
-
-	def __tester__( self, position, taille ): 
-		return self.source[position:position+taille] 
-
-	def __capture__( self, position, debut, fin=None ): 
-		if fin is None: 
-			fin = debut 
-		if self.source[position] != debut: 
-			return False 
-		echappement = False 
-		for i, signe in enumerate( self.source[position+1:] ): 
-			if signe == fin and echappement is False: 
-				return i+2 
-			elif signe == '\\': 
-				echappement = True 
-			elif echappement is True: 
-				echappement = False 
-		raise Exception( f"une capture a échoué ('{debut}')" ) 
-
-	def __entier__( self, position ): 
-		entiers = tuple( map( 
-			lambda i: str( i ), 
-			tuple( range( 0, 10 ) ) 
-		) ) 
-		if self.source[position] not in entiers: 
-			return False 
-		for i, signe in enumerate( self.source[position:] ): 
-			if signe not in entiers: 
-				return i 
-
 	def __init__( self, source ): 
 		self.source = source 
-		self.pointer = 0 
-		print( "condition = ", self.__condition__( 0 ) ) 
+		self.position = 0 
+		self.jetons = [] 
 
-	def __condition__( self, position ): 
+	def __iter__( self ): 
+		return self 
+
+	def __next__( self ): 
+		exp = self.__expression_variable__() 
+		if exp is not False: 
+			self.position, jeton = exp 
+			self.jetons.append( 
+				( "variable", jeton ) 
+			) 
+			return ( "variable", exp ) 
+		exp = self.__expression_condition__() 
+		if exp is not False: 
+			self.position, jeton = exp 
+			self.jetons.append( 
+				( "condition", jeton ) 
+			) 
+			return ( "condition", exp ) 
+		raise StopIteration 
+
+	def __expression_variable__( self ): 
 		jetons = []
+		position = self.position
+		position += self.__espaces__( position ) 
+		if "variable" != self.__tester__( position, 8 ).lower(): 
+			return False 
+		jetons.append( 
+			( 
+				"variable", 
+				"declaration" 
+			) 
+		) 
+		position += 8 
+		position += self.__espaces__( position ) 
+		r = self.__variable__( position ) 
+		if r is False: 
+			raise Exception( 
+				"une déclaration de variable n'est pas conforme (variable attendue)" 
+			) 
+		jetons.append( 
+			( 
+				"partie_gauche", 
+				self.source[position:position+r] 
+			) 
+		) 
+		position += r 
+		position += self.__espaces__( position ) 
+		if ":" != self.__tester__( position, 1 ): 
+			raise Exception( 
+				"un séparateur n'est pas trouvé lors de la déclaration d'une variable" 
+			) 
+		position +=1 
+		position += self.__espaces__( position ) 
+		r = self.__clause_operande__( position ) 
+		if r is False: 
+			raise Exception( 
+				"une opérande n'est pas trouvée lors de la déclaration d'une variable, après le séparateur" 
+			) 
+		p_relative, type_jeton = r 
+		jetons.append( 
+			( 
+				"partie_droite", 
+				( 
+					type_jeton, 
+					self.source[position:position+p_relative] 
+				) 
+			) 
+		) 
+		position += p_relative 
+		return position, jetons 
+
+	def __expression_condition__( self ): 
+		jetons = []
+		position = self.position
 		position += self.__espaces__( position ) 
 		if "condition" != self.__tester__( position, 9 ).lower(): 
 			return False 
@@ -92,7 +127,7 @@ class Traducteur:
 			jeton = self.__condition_liaison__( position ) 
 			if jeton is False: 
 				jetons.append( ( "condition", "fin" ) ) 
-				return jetons 
+				return position, jetons 
 			p_relative, jeton = jeton 
 			position += p_relative 
 			jetons.append( jeton ) 
@@ -101,6 +136,43 @@ class Traducteur:
 			if r is False: 
 				raise Exception( "un opérateur de liaison pour condition a été trouvé sans clause en suivant" ) 
 			jetons += r 
+
+	def __espaces__( self, position ): 
+		for i, signe in enumerate( self.source[position:] ): 
+			if signe in ( ' ', '\t', '\n' ): 
+				continue 
+			else: 
+				return i 
+		return i 
+
+	def __tester__( self, position, taille ): 
+		return self.source[position:position+taille] 
+
+	def __capture__( self, position, debut, fin=None ): 
+		if fin is None: 
+			fin = debut 
+		if self.source[position] != debut: 
+			return False 
+		echappement = False 
+		for i, signe in enumerate( self.source[position+1:] ): 
+			if signe == fin and echappement is False: 
+				return i+2 
+			elif signe == '\\': 
+				echappement = True 
+			elif echappement is True: 
+				echappement = False 
+		raise Exception( f"une capture a échoué ('{debut}')" ) 
+
+	def __entier__( self, position ): 
+		entiers = tuple( map( 
+			lambda i: str( i ), 
+			tuple( range( 0, 10 ) ) 
+		) ) 
+		if self.source[position] not in entiers: 
+			return False 
+		for i, signe in enumerate( self.source[position:] ): 
+			if signe not in entiers: 
+				return i 
 
 	def __condition_liaison__( self, position ): 
 		if "&" == self.__tester__( position, 1 ): 
@@ -198,28 +270,51 @@ class Traducteur:
 					return False 
 				return i+1 
 		raise Exception( 
-			"une variable a été commencés mais reste sans corps (fin de source)" 
+			"une variable a été commencée mais reste sans corps (fin de source)" 
 		) 
-
-
-
-
 
 ################################ 
 
-source = '''
+class Variable: 
+	def __init__( self, partie_gauche, partie_droite ): 
+		self.partie_gauche = partie_gauche 
+		self.partie_droite = partie_droite 
+
+################################ 
+
+source = ''' 
+
+	Variable $message : "bonjour" 
+
 	Condition "offre spéciale" (10) : 
 	  $date.aujourdhui < "2022-02-16" 
-	  & $client.est_membre()
+	  & $client.est_membre() 
 
 	Règle "réduction" (90) : 
 	  Si 
 	    ?"offre spéciale" 
 	  Alors 
 	    $panier.reduction( 10, "%" ) 
+
 ''' 
 
-# print( 
-Traducteur( source ) 
-# )
+t = Traducteur( source ) 
+for item in t:
+	print( 
+		item  
+	) 
+
+variables = [ 
+	Variable( variable[1], variable[2] ) 
+	for variable in [ 
+		jeton[1] 
+		for jeton in t.jetons 
+		if jeton[0] == "variable" 
+	] 
+]
+
+print( 
+	"---\n", 
+	variables 
+) 
 
