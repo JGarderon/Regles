c1d1ff66d4f89c5aacf918094fe370f8fda0ce69
Administrator <julien.garderon@gmail.com>
2022-02-14 22:30:21 +0100

    Début de la jonction variable d'environnement / type de résolution

--- app-poc-rust/src/main.rs
+++ app-poc-rust/src/main.rs
@@ -1,50 +1,78 @@
 #![allow(warnings, unused)] 
 
-mod clauses; 
-mod jetons; 
-mod feuilles; 
-mod regles; 
-mod conditions; 
+use std::env; 
 
-use crate::clauses::Clause; 
-use crate::jetons::Jeton; 
-use crate::feuilles::Feuille; 
-use crate::regles::Regle; 
-use crate::conditions::Condition; 
+mod resolution_traitement; 
+use crate::resolution_traitement::resolution_traitement; 
 
-fn main() {
+static ENV_RESOLUTION: &'static str = "RESOLUTION_TYPE"; 
 
-	let liste = vec!( 
-		Jeton::GroupeOuvrant, 
-			Jeton::Appelable( Clause::creer( "a".to_string() ) ), 
-			Jeton::LiaisonOu, 
-			Jeton::Appelable( Clause::creer( "b".to_string() ) ), 
-			Jeton::LiaisonEt, 
-			Jeton::Appelable( Clause::creer( "c".to_string() ) ), 
-		Jeton::GroupeFermant, 
-		Jeton::LiaisonEt, 
-		Jeton::GroupeOuvrant, 
-			Jeton::Appelable( Clause::creer( "d".to_string() ) ), 
-			Jeton::LiaisonEt, 
-			Jeton::GroupeOuvrant, 
-				Jeton::Appelable( Clause::creer( "e".to_string() ) ), 
-				Jeton::LiaisonOu, 
-				Jeton::Appelable( Clause::creer( "f".to_string() ) ), 
-			Jeton::GroupeFermant, 
-		Jeton::GroupeFermant, 
-		Jeton::LiaisonEt, 
-		Jeton::Appelable( Clause::creer( "g".to_string() ) ) 
-	); 
+fn main() { 
 
-	let mut condition = Condition::creer( liste ); 
-	condition.resoudre(); 
+	match env::var( ENV_RESOLUTION ) {
+		Ok( resolution ) => match &resolution[..] { 
+			"traitement" => resolution_traitement(), 
+			indetermine => panic!( 
+				"La résolution {:?} n'est pas supportée", 
+				indetermine 
+			) 
+		} 
+		Err( erreur ) => panic!( 
+			"La variable déclarant la résolution a rencontré une erreur : {:?}", 
+			erreur
+		) 
+	} 
 
-	println!( 
-		"résultat final = {:?}",  
-		condition.etat 
-	); 
+}
 
-} 
+
+/*	Version de réserve 	*/ 
+
+// mod clauses; 
+// mod jetons; 
+// mod feuilles; 
+// mod regles; 
+// mod conditions; 
+
+// use crate::clauses::Clause; 
+// use crate::jetons::Jeton; 
+// use crate::feuilles::Feuille; 
+// use crate::regles::Regle; 
+// use crate::conditions::Condition; 
+
+// fn main() {
+
+// 	let liste = vec!( 
+// 		Jeton::GroupeOuvrant, 
+// 			Jeton::Appelable( Clause::creer( "a".to_string() ) ), 
+// 			Jeton::LiaisonOu, 
+// 			Jeton::Appelable( Clause::creer( "b".to_string() ) ), 
+// 			Jeton::LiaisonEt, 
+// 			Jeton::Appelable( Clause::creer( "c".to_string() ) ), 
+// 		Jeton::GroupeFermant, 
+// 		Jeton::LiaisonEt, 
+// 		Jeton::GroupeOuvrant, 
+// 			Jeton::Appelable( Clause::creer( "d".to_string() ) ), 
+// 			Jeton::LiaisonEt, 
+// 			Jeton::GroupeOuvrant, 
+// 				Jeton::Appelable( Clause::creer( "e".to_string() ) ), 
+// 				Jeton::LiaisonOu, 
+// 				Jeton::Appelable( Clause::creer( "f".to_string() ) ), 
+// 			Jeton::GroupeFermant, 
+// 		Jeton::GroupeFermant, 
+// 		Jeton::LiaisonEt, 
+// 		Jeton::Appelable( Clause::creer( "g".to_string() ) ) 
+// 	); 
+
+// 	let mut condition = Condition::creer( liste ); 
+// 	condition.resoudre(); 
+
+// 	println!( 
+// 		"résultat final = {:?}",  
+// 		condition.etat 
+// 	); 
+
+// } 
 
 
 
---
+++ app-poc-rust/src/resolution_traitement.rs
@@ -0,0 +1,34 @@
+
+use std::env; 
+use std::fs; 
+
+static ENV_VAR_CHEMIN: &'static str = "SOURCE_CHEMIN"; 
+
+pub fn resolution_traitement() { 
+	
+	let mut fichiers_source: Vec<String> = vec!(); 
+	match env::var( ENV_VAR_CHEMIN ) {
+		Ok( chemin ) => match fs::read_dir( &chemin ) {
+			Ok( curseur ) => for fichier in curseur { 
+				fichiers_source.push( 
+					fichier.unwrap().path().display().to_string() 
+				); 
+			}, 
+			Err( _ ) => panic!( 
+				"Le chemin {:?} n'est pas accessible", 
+				chemin 
+			) 
+		},
+		Err( erreur ) => panic!( 
+			"Erreur lors de la lecture de la variable d'environnement {:?} : {:?}", 
+			ENV_VAR_CHEMIN, 
+			erreur 
+		) 
+	} 
+
+	println!("{:?}", fichiers_source); 
+
+} 
+
+
+
